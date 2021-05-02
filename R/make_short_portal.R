a47646cad7b1f65bfb72f65832ab2def63508bb0
Renata Diaz <diaz.renatam@gmail.com>
2021-05-02 13:08:08 -0400

    plants annual fxn

--- R/make_short_portal.R
+++ R/make_short_portal.R
@@ -32,3 +32,35 @@ abund_dat <- list(abundance = abundance,
 return(abund_dat)
 
 }
+
+#' Get Portal plants
+#'
+#' @param census_season "winter" or summmer
+#'
+#' @return ready for ldats
+#' @export
+#'
+#' @importFrom portalr plant_abundance
+#' @importFrom dplyr filter select mutate
+#' @importFrom tidyr pivot_wider
+get_plants_annual <- function(census_season = "winter") {
+
+
+  if(census_season == "winter") {
+  quadrats <- portalr::plant_abundance(level = "Treatment", type = "Winter Annuals", plots = "longterm")
+  } else {
+    quadrats <- portalr::plant_abundance(level = "Treatment", type = "Summer Annuals", plots = "longterm")
+
+}
+
+  quadrats_wide <- quadrats %>%
+    dplyr::filter(treatment == "control",
+                  season ==census_season,
+                  quads == 64) %>%
+    tidyr::pivot_wider(id_cols = year, names_from = species, values_from = abundance, values_fill = 0)
+
+  abundance <- dplyr::select(quadrats_wide, -year)
+  covariates <- dplyr::select(quadrats_wide, year) %>%
+    dplyr::mutate(col2 = "dummycol")
+
+  }
